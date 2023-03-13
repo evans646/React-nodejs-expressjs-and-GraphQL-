c678813876e986874adabb37bfe6d04db87821ab
Evans Ansong <65941289+evans646@users.noreply.github.com>
2023-03-13 03:49:05 +0000

    feat: initial state

---
+++ api/src/index.js
@@ -0,0 +1,17 @@
+//to start the server and  configure routes 
+import express from 'express';
+const app = express();
+const port =2020;
+
+
+
+
+app.listen(port,() =>{
+    console.log(`Server is listening on port ${port}`);
+});
+
+
+
+app.listen(port,() =>{
+    console.log(`Server is listening on port ${port}`)
+});
